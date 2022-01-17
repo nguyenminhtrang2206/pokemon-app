756bd7ca4a7b673d030a21f440c3a0dcd540ee7f
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2022-01-17 12:31:33 +0200

    cleaning app and adding navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <>
+     <Container>
+  <Navbar variant="dark" bg="dark">
+    <Container>
+      <Navbar.Brand href="#">Pokemon App</Navbar.Brand>
+    </Container>
+  </Navbar>
+</Container>
+</>
+
     </div>
   );
-}
+};
 
 export default App;
