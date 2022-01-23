17c1fa36a72103849e9c4fad1d013ce6d965cbe8
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2022-01-23 17:27:38 +0200

    adding api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import './App.css';
+import './Card'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
+
+
 
 const App = () => {
+  const [pokemons, setPokemons] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/").then((res) => {
+      const fetches = res.data.results.map((p) =>
+        axios.get(p.url).then((res) => res.data)
+      );
+
+      Promise.all(fetches).then((data) => {
+        setPokemons(data);
+        setIsLoading(false);
+      });
+    });
+  }, []);
+
+
   return (
     <div>
-     <>
-     <Container>
-  <Navbar variant="dark" bg="dark">
-    <Container>
-      <Navbar.Brand href="#">Pokemon App</Navbar.Brand>
-    </Container>
-  </Navbar>
-</Container>
-</>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#">PokeApp</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row
+          xs={2}
+          md={4}
+          lg={5}
+          className="justify-content-between my-5 d-flex gap-3"
+        >
+          {isLoading && (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          )}
+          {!isLoading &&
+            pokemons.map((pokemon) => (
+              <Card bg="dark" text="light" key={pokemon.name}>
+                <Card.Header>{pokemon.name}</Card.Header>
 
+                <Card.Body>
+                  <Card.Img
+                    variant="top"
+                    src={pokemon.sprites.other.dream_world.front_default}
+                  />
+                </Card.Body>
+              </Card>
+            ))}
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg" />
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Button variant="primary">Read more</Button>
+  </Card.Body>
+</Card>
