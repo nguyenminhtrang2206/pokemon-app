2d599fb682ec8ce9376910620b7eb9dbb77b42bd
BC-macair020 <bc-macair020@BC-macair020s-MacBook-Air.local>
2022-01-24 13:31:16 +0200

    hero-banner in Home page

--- src/Home.js
+++ src/Home.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className="display-1">
-            Pokemon App
+        <div className="hero d-flex justify-content-center align-items-center ">
+            <h1 className="display-1 heading">Pokemon App</h1>
+            
         </div>
     );
 };
